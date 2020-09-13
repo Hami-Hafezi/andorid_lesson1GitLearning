a8b701463cb4d8e14e29d8ebe2597c6577cd2ce7
Hami <dev.hamihafezi@gmail.com>
2020-09-14 00:02:47 +0430

    first commit

---
+++ app/src/main/java/com/hami/githublearn1/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.hami.githublearn1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val s:String
+        val s1:String? = null
+        val ss = s1
+
+    }
+}
